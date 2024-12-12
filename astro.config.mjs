1d9e43f20191a47e8543bd6084c0f50b2db5f31f
Rodrigo Javier <53836460+RodrigoJavier101@users.noreply.github.com>
2024-12-12 20:40:21 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://RodrigoJavier101.guthub.io',
     integrations: [
         mdx(),
         sitemap(),
